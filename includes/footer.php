93cc07afb12df7aa1fe12370f78e10f295d22c34
joedigitalacad <47310368+joedigitalacad@users.noreply.github.com>
2019-03-09 06:43:11 +0100

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+<footer>
+		<div class="container">
+			<div class="row">
+				<!-- <p style="text-align: center;">Copyright @ <?php date('Y');?> <a href="joedigitalacademy.com">Joe Digital Acad</a></p> -->
+			</div>
+		</div>
+	</footer>
+</div><!-- wrapper -->
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Stock Management System</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-grid.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-reboot.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css.map">
+	<script type="text/javascript" src="vendor/jquery-1.12.4.min.js"></script>
+	<script type="text/javascript" src="vendor/modernizr-2.8.3.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+	<script type="text/javascript" src="js/bootstrap.bundle.js"></script>
+</head>
+<body style="background: #ccc;">
